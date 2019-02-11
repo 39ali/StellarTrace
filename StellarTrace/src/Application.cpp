5c04b4f5ca708578c3cd5c006814757e2813427b
alimilhim <alimilhim5@gmail.com>
2019-02-11 02:24:25 +0200

    added depth of field

--- StellarTrace/src/Application.cpp
+++ StellarTrace/src/Application.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STBI_MSC_SECURE_CRT
+#include <cstdlib>
+#include <limits>
+#include <random>
 #include "../vendor/stb_image_write.h"
 #include "Camera.h"
 #include "Core.h"
@@ -13,9 +16,6 @@
 #include "Metal.h"
 #include "Ray.h"
 #include "Sphere.h"
-#include <cstdlib>
-#include <limits>
-#include <random>
 //#include <intrin.h>
 
 // simd
@@ -34,7 +34,6 @@ vec3 random_sphere() {
   return p;
 }
 vec3 color(const Ray &r, HitableList *world, int depth) {
-
   HitRecord rec;
 
   if (world->Hit(r, 0.01, std::numeric_limits<float>::max(), rec)) {
@@ -50,32 +49,74 @@ vec3 color(const Ray &r, HitableList *world, int depth) {
   float t = 0.5 * (dir.y + 1.0);
   return (1.0 - t) * vec3(1) + (t)*vec3(0.5, 0.7, 1.0);
 }
+
+#define DRAND (float)std::rand() / RAND_MAX
+HitableList objects;
+void RandomScene() {
+  const int c = 5;
+  objects.Add(new Sphere(vec3(0, -1000, 0), 1000, new Lambertian(vec3(0.5))));
+  int i = 1;
+  for (int a = -c; a < c; a++) {
+    for (int b = -c; b < c; b++) {
+      float chooseMat = DRAND;
+      vec3 center(a + 0.9 * DRAND, 0.2, b + 0.9 * DRAND);
+      if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+        if (chooseMat < 0.8)  // diffuse
+          objects.Add(new Sphere(center, 0.2, new Lambertian(vec3(DRAND * DRAND, DRAND * DRAND,DRAND * DRAND))));
+        else if (chooseMat < 0.95)  // metal
+          objects.Add(new Sphere(
+              center, 0.2, new Metal(vec3(0.5 * (1 + DRAND * DRAND), 0.5 * (1 + DRAND * DRAND), 0.5 * (1 + DRAND * DRAND)), 0.3 * DRAND)));
+        else
+          objects.Add(new Sphere(center, 0.2, new Dielectric(1.5)));
+      }
+    }
+  }
+
+  objects.Add(new Sphere(vec3(0, 1, -1), 1, new Dielectric(1.5)));
+  objects.Add(
+      new Sphere(vec3(-4, 1, -1), 1, new Lambertian(vec3(0.8, 0.8, 0.0))));
+  objects.Add(
+      new Sphere(vec3(4, 0, -1), 1.5, new Metal(vec3(0.8, 0.6, 0.2), 0.1)));
+}
+#include <thread>
 int main() {
-
+	auto begin = std::chrono::steady_clock::now();
   //	I64vec1 sss(3, 3);
-
+	unsigned concurentThreadsSupported = std::thread::hardware_concurrency();
   float w = 300, h = 200;
   int samples = 8;
   uint8_t *data1 = new uint8_t[w * h * 3];
 
-  HitableList *world = new HitableList();
-  /* float R = cosf(3.14 / 4);
-   world->Add(
-       new Sphere(vec3(0, 0, -1),0.5, new Lambertian(vec3(0.0, 0.0, 1))));
-   world->Add(
-           new Sphere(vec3(-1, 0, -1), 0.5, new Lambertian(vec3(1.0, 0.0, 1))));
-   */ 
-  world->Add(new Sphere(vec3(0, 0, -1), 0.5,
-                              new Lambertian(vec3(0.8, 0.3, 0.3))));
-  world->Add(new Sphere(vec3(0, -100.5, -1), 100,
-                        new Lambertian(vec3(0.8, 0.8, 0.0))));
-  world->Add(
-      new Sphere(vec3(1, 0, -1), 0.5, new Metal(vec3(0.8, 0.6, 0.2), 0.1)));
-  world->Add(new Sphere(vec3(-1, 0, -1), 0.5, new Dielectric(1.5)));
-  world->Add(new Sphere(vec3(-1, 0, -1), -0.45, new Dielectric(1.5)));
+
   
-  Camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 35.0f,
-             float(w) / float(h));
+
+
+  
+  // HitableList *world = new HitableList();
+  ///* float R = cosf(3.14 / 4);
+  // world->Add(
+  //     new Sphere(vec3(0, 0, -1),0.5, new Lambertian(vec3(0.0, 0.0, 1))));
+  // world->Add(
+  //         new Sphere(vec3(-1, 0, -1), 0.5, new Lambertian(vec3(1.0, 0.0,
+  //         1))));
+  // */
+
+  ///*world->Add(
+  //    new Sphere(vec3(0, 0, -1), 0.5, new Lambertian(vec3(0.8, 0.3, 0.3))));
+  // world->Add(new Sphere(vec3(0, -100.5, -1), 100,
+  //                      new Lambertian(vec3(0.8, 0.8, 0.0))));
+  // world->Add(
+  //    new Sphere(vec3(1, 0, -1), 0.5, new Metal(vec3(0.8, 0.6, 0.2), 0.1)));*/
+  // world->Add(new Sphere(vec3(-1, 0, -1), 0.5, new Dielectric(1.5)));
+  // world->Add(new Sphere(vec3(-1, 0, -1), -0.45, new Dielectric(1.5)));
+  RandomScene();
+  const vec3 eye(13, 3, 5);
+  const vec3 lookAt(0, 0, -1);
+  float distToFocus = (eye - lookAt).length();
+  float aperture = 0.0;
+
+  Camera cam(eye, lookAt, vec3(0, 1, 0), 35.0f, float(w) / float(h), aperture,
+             distToFocus);
 #if 1
   for (int y = 0; y < h; y++) {
     for (int x = 0; x < w; x++) {
@@ -85,7 +126,7 @@ int main() {
         const float u = float(x + k) / w;
         const float v = float(y + k) / h;
         Ray r = cam.GetRay(u, v);
-        col += color(r, world, 0);
+        col += color(r, &objects, 0);
       }
       col /= float(samples);
       const int pos = y * w * 3 + x * 3;
@@ -97,4 +138,7 @@ int main() {
 #endif
   stbi_flip_vertically_on_write(1);
   stbi_write_png("f1.png", w, h, 3, data1, w * 3);
+  auto end = std::chrono::steady_clock::now();
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << std::endl;
 }
--- StellarTrace/src/Camera.h
+++ StellarTrace/src/Camera.h
@@ -1,34 +1,48 @@
 #pragma once
 #include "Ray.h"
 #define PIE 3.14f
+#include <random>
 namespace StellarTrace {
 
+vec3 random_in_unit_disk() {
+  vec3 p;
+  do {
+	  p = 2.0 * vec3(float(std::rand()) / RAND_MAX, float( std::rand()) / RAND_MAX, 0) -
+        vec3(1, 1, 0);
+  } while (p.dot(p) >= 1.0);
+  return p;
+}
+
 class Camera {
-public:
+ public:
   // fov is in degrees also it's vertical
   Camera(const vec3 &eye, const vec3 &lookAt, const vec3 &up, float fov,
-         float aspectRatio) {
-    vec3 u, v, w;
+         float aspectRatio, float aperture, float focus_dist) {
+    lens_r = aperture / 2;
     float th = fov * PIE / 180.f;
     float halfH = tanf(th / 2);
     float halfW = aspectRatio * halfH;
-	Origin = eye;
-	w = (eye - lookAt).normalize();
-	u = up.cross(w).normalize();
-	v = w.cross(u);
-    //LowerLeftCorner = vec3(-halfW, -halfH, -1);
-	LowerLeftCorner = Origin - halfW * u - halfH * v - w;
-	Horizontal = 2 * halfW*u;
-    Vertical = 2 * halfH*v;
-   
+    Origin = eye;
+    w = (eye - lookAt).normalize();
+    u = up.cross(w).normalize();
+    v = w.cross(u);
+    // LowerLeftCorner = vec3(-halfW, -halfH, -1);
+    LowerLeftCorner = Origin - halfW * u*focus_dist - halfH * v*focus_dist - w*focus_dist;
+    Horizontal = 2 * halfW * u*focus_dist;
+    Vertical = 2 * halfH * v*focus_dist;
   }
-  inline Ray GetRay(float u, float v) const {
-    return Ray(Origin, LowerLeftCorner + u * Horizontal + v * Vertical -Origin);
+  inline Ray GetRay(float s, float t) const {
+	  vec3 rd = lens_r * random_in_unit_disk();
+	  vec3 offset = u * rd.x + v * rd.y;
+    return Ray(Origin + offset,
+               LowerLeftCorner + s * Horizontal + t * Vertical - Origin - offset);
   }
   vec3 Origin;
   vec3 LowerLeftCorner;
   vec3 Horizontal;
   vec3 Vertical;
+  vec3 u, v, w;
+  float lens_r;
 };
 
-} // namespace StellarTrace
+}  // namespace StellarTrace
